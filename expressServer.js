81d83009d57f000cdb362e7f75e9e4c3dc4018f8
gwanwoo-you <skymill2000@gmail.com>
2020-07-14 11:48:13 +0900

    회원가입 페이지 ㄷ추가

--- expressServer.js
+++ expressServer.js
@@ -9,37 +9,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
-
-app.get("/hello", function (req, res) {
-  res.send("안녕하세요 !");
-});
-
-app.get("/htmlTest", function (req, res) {
-  res.send("<html><h1>안녕하세요</h1><hr/><p>만들기 어렵습니다</p></html>");
-});
-
-app.get("/ejsTest", function (req, res) {
-  res.render("test");
-});
-
-app.get("/inputTest", function (req, res) {
-  res.render("inputTest");
-});
-
-app.get("/designTest", function (req, res) {
-  res.render("design");
-});
-
-app.post("/getLoginData", function (req, res) {
-  var userId = req.body.ajaxUserId;
-  var userPassword = req.body.ajaxUserPassword;
-  console.log("req body : ", req.body);
-  console.log(userId, userPassword);
-
-  res.json(userId + "분의 로그인 성공입니다.");
+app.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
 app.listen(3000, function () {
