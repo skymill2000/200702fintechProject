0e8277e8275f72899c0333c0859b20e0d3c7f369
gwanwoo-you <skymill2000@gmail.com>
2020-07-14 13:38:18 +0900

    회원 인증후 토큰 요청 만들기 완료

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const request = require("request");
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
@@ -11,6 +12,33 @@ app.use(express.static(__dirname + "/public"));
 
 app.get("/signup", function (req, res) {
   res.render("signup");
+});
+
+app.get("/authResult", function (req, res) {
+  var authCode = req.query.code;
+  console.log("인증코드 : ", authCode);
+
+  var option = {
+    method: "POST",
+    url: "https://testapi.openbanking.or.kr/oauth/2.0/token",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    //form 형태는 form / 쿼리스트링 형태는 qs / json 형태는 json ***
+    form: {
+      code: authCode,
+      client_id: "q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF",
+      client_secret: "yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy",
+      redirect_uri: "redirect_uri",
+      grant_type: "authorization_code",
+      //#자기 키로 시크릿 변경
+    },
+  };
+
+  request(option, function (error, response, body) {
+    console.log(body);
+    res.json(body);
+  });
 });
 
 app.listen(3000, function () {
