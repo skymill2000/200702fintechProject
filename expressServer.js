3b37cedf925ee0b7e33544063da7fbbfbae0863d
gwanwoo-you <skymill2000@gmail.com>
2020-07-16 11:10:22 +0900

    잔액조회 프론트 페이지 추가

--- expressServer.js
+++ expressServer.js
@@ -40,6 +40,10 @@ app.get("/main", function (req, res) {
   res.render("main");
 });
 
+app.get("/balance", function (req, res) {
+  res.render("balance");
+});
+
 app.get("/authResult", function (req, res) {
   var authCode = req.query.code;
   console.log("인증코드 : ", authCode);
