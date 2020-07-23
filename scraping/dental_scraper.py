f5b32b0124e415aa2f6562fbf4d5bbb85dfad439
gwanwoo-you <skymill2000@gmail.com>
2020-07-23 15:34:53 +0900

    치아보험 AIA 작성

--- scraping/dental_scraper.py
+++ scraping/dental_scraper.py
@@ -13,25 +13,42 @@ def rePlaceData(value):
     return result
 
 
-driver.get(
-    'https://direct.lina.co.kr/product/ess/dtc01/easy')
-birthInput = driver.find_element_by_xpath('//*[@id="birthday"]')
-maleBtn = driver.find_element_by_xpath('//*[@id="main_btn_male"]')
-resultBtn = driver.find_element_by_xpath('//*[@id="btn_direct_dental_cal"]')
+def getAIAData(name, birth, gender):
 
-birthInput.send_keys('890119')
-maleBtn.click()
-resultBtn.click()
-driver.implicitly_wait(3)
-htmlResult = driver.find_element_by_xpath('//*[@id="mo_amount_span"]').text
-resultValue = rePlaceData(htmlResult)
-detailBtn = driver.find_element_by_xpath('//*[@id="openLayerplanPonA2"]')
-detailBtn.click()
-table = driver.find_element_by_xpath(
-    '//*[@id="planPonA2"]/div/div[2]/div/div/table[1]/tbody')
-tableRow = table.find_elements_by_tag_name('tr')
-for index, value in enumerate(tableRow):
-    title = value.find_elements_by_tag_name('th')[0].text
-    print(title)
 
-print(htmlResult)
+def getLinaData(name, birth, gender):
+    driver = webdriver.Chrome('./chromedriver')
+    scrapingResult = {
+        'company': "라이나",
+        'price': 0,
+        'contents': []
+    }
+    driver.get('https://direct.lina.co.kr/product/ess/dtc01/easy')
+    textBox = driver.find_element_by_xpath('//*[@id="birthday"]')
+    textBox.send_keys(birth)
+    if gender == 1:
+        femaleBtn = driver.find_element_by_xpath('//*[@id="main_btn_female"]')
+        femaleBtn.click()
+    else:
+        maleBtn = driver.find_element_by_xpath('//*[@id="main_btn_male"]')
+        maleBtn.click()
+    resultBtn = driver.find_element_by_xpath(
+        '//*[@id="btn_direct_dental_cal"]')
+    resultBtn.click()
+    htmlResult = driver.find_element_by_xpath('//*[@id="mo_amount_span"]').text
+    resultValue = rePlaceData(htmlResult)
+    scrapingResult['price'] = resultValue
+    driver.implicitly_wait(1)
+    detailBtn = driver.find_element_by_xpath('//*[@id="openLayerplanPonA2"]')
+    detailBtn.click()
+    tableBody = driver.find_element_by_xpath(
+        '//*[@id="planPonA2"]/div/div[2]/div/div/table[1]').find_element_by_tag_name('tbody')
+    rows = tableBody.find_elements_by_tag_name("tr")
+    contentsList = []
+    for index, value in enumerate(rows):
+        if index != 0:
+            print(value.find_elements_by_tag_name('th')[0].text)
+            contentsList.append(value.find_elements_by_tag_name('th')[
+                                0].text)
+    scrapingResult['contents'] = contentsList
+    return scrapingResult
