047f68a7308a105dee132637319c900054bb742c
gwanwoo-you <skymill2000@gmail.com>
2020-07-23 13:36:05 +0900

    멜론차트 스크래핑 완성

--- scraping/MelonChart.py
+++ scraping/MelonChart.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 driver = webdriver.Chrome('./chromedriver')
-# https://www.melon.com/chart/index.htm
+driver.get('https://www.melon.com/chart/index.htm')
+
+table = driver.find_element_by_xpath(
+    '//*[@id="frm"]/div/table/tbody').find_elements_by_tag_name('tr')
+
+for index, value in enumerate(table):
+    no = value.find_elements_by_tag_name('td')[0]
+    rowNo = value.find_elements_by_tag_name('td')[1]
+    address = value.find_elements_by_tag_name('td')[2]
+    homeType = value.find_elements_by_tag_name('td')[3]
+    print(no.text + rowNo.text + address.text + homeType.text)
+
+
+# //*[@id="frm"]/div/table/tbody
