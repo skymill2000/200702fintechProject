7375ec438f2e10610333097b5f9f93c66947945b
gwanwoo-you <skymill2000@gmail.com>
2020-07-23 10:16:22 +0900

    노드 스크래퍼

---
+++ test.js
@@ -0,0 +1,13 @@
+{
+    "cntr_account_type": "N", "cntr_account_num": "00012345678901234", "wd_pass_phrase": "790d56ed........6b821a69", "wd_print_content": "환불금액", "name_check_option": "on", "sub_frnc_name": "하위가맹점", "sub_frnc_num": "123456789012", "sub_frnc_business_num": "1234567890", "tran_dtime": "20190910101921",
+    "req_cnt": "25",
+    "req_list": [
+    {
+    "tran_no": "1",
+    "bank_tran_id": "F123456789U4BC34239Z", "fintech_use_num": "123456789012345678901234", "print_content": "쇼핑몰환불",
+    "tran_amt": "10000",
+    "req_client_name": "홍길동", "req_client_bank_code": "097", "req_client_account_num": "00012300000678", "req_client_num": "HONGGILDONG1234", "transfer_purpose": "TR",
+    "recv_bank_tran_id": "F123456789U4BCT4112Z", "cms_num": "93848103221"
+    },
+    { ... }, ... {...}
+    ] }
