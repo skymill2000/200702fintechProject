13a6781b626d54f12118f2aa8bda828b7a62f1bc
gwanwoo-you <skymill2000@gmail.com>
2020-07-23 15:47:58 +0900

    치아보험 AIA 완성

--- scraping/dental_scraper.py
+++ scraping/dental_scraper.py
@@ -16,12 +16,50 @@ def rePlaceData(value):
 def getAIAData(name, birth, gender):
     driver = webdriver.Chrome('./chromedriver')
     scrapingResult = {
-        'company': "라이나",
+        'company': "AIA",
         'price': 0,
         'contents': []
     }
+
+    driver.implicitly_wait(3)
     driver.get(
         'https://www.aia.co.kr/ko/our-products/medical-protection/non-par-denteal-health-plan.html#')
+    driver.implicitly_wait(3)
+    # name.decode('utf-8').encode('euc-kr')
+    textBox = driver.find_element_by_xpath('//*[@id="aia644363719"]')
+    textBox.send_keys("19"+birth)
+    if gender == 1:
+        femaleBtn = driver.find_element_by_xpath(
+            '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[1]')
+        # '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[1]
+
+        femaleBtn.click()
+    else:
+        maleBtn = driver.find_element_by_xpath(
+            '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[2]')
+        maleBtn.click()
+    resultBtn = driver.find_element_by_xpath('//*[@id="btn806817556"]')
+    resultBtn.click()
+    driver.implicitly_wait(3)
+
+    htmlResult = driver.find_element_by_xpath(
+        '//*[@id="premium-by-timespan-value"]').text
+    resultValue = rePlaceData(htmlResult)
+    print(resultValue)
+    scrapingResult['price'] = resultValue
+    tableBody = driver.find_element_by_xpath(
+        '//*[@id="collapse-large-724022276"]/div[1]/div/table').find_element_by_tag_name('tbody')
+    driver.find_element_by_xpath(
+        '//*[@id="the_fine_print"]/div[2]/div[1]/div[2]/div/a[2]').click()
+    rows = tableBody.find_elements_by_tag_name("tr")
+    contentsList = []
+    for index, value in enumerate(rows):
+        if index != 0:
+            print(value.find_elements_by_tag_name('td')[0].text)
+            contentsList.append(value.find_elements_by_tag_name('td')[
+                                0].text)
+    scrapingResult['contents'] = contentsList
+    return scrapingResult
 
 
 def getLinaData(name, birth, gender):
@@ -60,3 +98,6 @@ def getLinaData(name, birth, gender):
                                 0].text)
     scrapingResult['contents'] = contentsList
     return scrapingResult
+
+
+getAIAData('오', '890119', 0)
