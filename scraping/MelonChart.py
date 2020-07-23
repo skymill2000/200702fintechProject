44b0d20b89abe39af8e5f09baf4c9fb55491b240
gwanwoo-you <skymill2000@gmail.com>
2020-07-23 13:21:42 +0900

    멜론차트 스크래핑 만들기

---
+++ scraping/MelonChart.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+driver = webdriver.Chrome('./chromedriver')
+# https://www.melon.com/chart/index.htm
--- scraping/TableScraping.py
+++ scraping/TableScraping.py
@@ -7,7 +7,16 @@ driver.implicitly_wait(1)
 table = driver.find_element_by_class_name('search_list')
 tableBody = table.find_element_by_tag_name('tbody')
 tablerow = tableBody.find_elements_by_tag_name('tr')
-for index, value in enumerate(tablerow):
-    if index != 0:
-        addDate = value.find_elements_by_tag_name('td')[2].text
-        print(addDate)
+for i in range(4, 14):
+    driver.implicitly_wait(1)
+    for index, value in enumerate(tablerow):
+        if index != 0:
+            type = value.find_elements_by_tag_name('td')[1].text
+            address = value.find_elements_by_tag_name('td')[2].text
+            pay = value.find_elements_by_tag_name('td')[3].text
+            size = value.find_elements_by_tag_name('td')[4].text
+            jimk = value.find_elements_by_tag_name('td')[5].text
+            print(type,  address, pay, size, jimk)
+    element = driver.find_element_by_xpath(
+        '//*[@id="list"]/tbody/tr[4]/td/ul/a['+str(i)+']')
+    driver.execute_script("arguments[0].click();", element)
